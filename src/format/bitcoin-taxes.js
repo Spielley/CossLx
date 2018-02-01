b39611ec4ab7f19b7712c0b7b4c07dfab48e53e5
Alexandre Potvin Latreille (plalx) <alexandre.potvin.latreille@gmail.com>
2018-01-31 21:10:55 -0500

    Remove unnecessary conditionnal transaction type resolution

--- src/format/bitcoin-taxes.js
+++ src/format/bitcoin-taxes.js
@@ -1,6 +1,6 @@
 import { LINE_BREAK, formatUtcDate } from './util';
 import { isTrade } from '../filter';
-import { TransactionType, exchangeSource } from '../core'
+import { exchangeSource } from '../core'
 
 const data = ['Date,Action,Source,Symbol,Volume,Price,Currency,Fee,FeeCurrency'];
 
@@ -9,7 +9,7 @@ export default (transactions) => {
     const rowData = transactions.filter(isTrade).map(function (transaction) {
         return [
             formatUtcDate(transaction.occuredOn),
-            transaction.type === TransactionType.BUY? 'BUY' : 'SELL',
+            transaction.type.toString().toUpperCase(),
             exchangeSource,
             transaction.baseCurrency,
             transaction.amount,
